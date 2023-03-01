158f7d53a4925f998f57eea3e852db5079a8d557
JeromeJULIEN <102427619+JeromeJULIEN@users.noreply.github.com>
2023-03-01 09:54:26 +0100

    smart contract update

--- hardhat/contracts/collectorChain.sol
+++ hardhat/contracts/collectorChain.sol
@@ -6,19 +6,40 @@ import "../node_modules/@openzeppelin/contracts/token/ERC1155/extensions/ERC1155
 import "../node_modules/@openzeppelin/contracts/access/Ownable.sol";
 
 contract CollectorChain is ERC1155URIStorage, Ownable {
+    /// @dev init of URI param
     string constant collectionURI_ = "";
-    uint256 private _id;
+    /// @dev init of _id param to use for nft id counter during mint and stocker id counter during stoker creation
+    uint256 public _nftIdCounter;
+    uint256 public _stockerIdCounter;
+
+    /// @dev init of royaltee param at 5%
+    uint8 public royaltee = 5;
+
+    enum Status {
+        proposed,
+        accepted,
+        rejected,
+        minted,
+        burned
+    }
 
     /// @notice main information of the NFT
     struct Nft {
         address minter;
-        string NftName;
-        uint256 StockerId;
-        uint256 value;
+        string nftName;
+        uint256 stockerId;
+        Status status;
     }
 
-    /// @notice list of all the NFT related to their ID
+    struct Stocker {
+        address stockerAddr;
+        string stockerName;
+    }
+
+    /// @notice mapping of all the NFT related to their ID
     mapping(uint256 => Nft) nftList;
+
+    mapping(uint256 => Stocker) stockerList;
 
     constructor() ERC1155(collectionURI_) {}
 
@@ -31,4 +52,16 @@ contract CollectorChain is ERC1155URIStorage, Ownable {
 
     /// @notice mint the NFT once the proposal accepted
     function mintNft() external {}
+
+    /// @notice creation of a new stocker
+    /// @notice only for contract owner
+    function stockerCreation(address _stockerAddr, string memory _stockerName)
+        external
+        onlyOwner
+    {
+        uint256 currentStockerId = _stockerIdCounter;
+        Stocker memory newStocker = Stocker(_stockerAddr, _stockerName);
+        stockerList[currentStockerId] = newStocker;
+        _stockerIdCounter++;
+    }
 }
