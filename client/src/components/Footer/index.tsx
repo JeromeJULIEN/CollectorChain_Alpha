fed4f3fbfb0c3c709c509378a509a7e26dab8650
JeromeJULIEN <102427619+JeromeJULIEN@users.noreply.github.com>
2023-03-10 16:11:07 +0100

    home page ok

--- client/src/components/Footer/index.tsx
+++ client/src/components/Footer/index.tsx
@@ -1,10 +1,26 @@
 import React from 'react'
+import './styles.scss'
+import logo from "../../image/logo.png"
+
 
 type Props = {}
 
 const Footer = (props: Props) => {
   return (
-    <div>Footer</div>
+    <div className='footer'>
+        <div className="footer__logo">
+            <img src={logo} alt="logo" />
+        </div>
+        <div className="footer__text">
+            <div>collector chain @2023</div>
+            <div className="footer__text--link">
+                <div>Contact us</div>
+                <div>Create</div>
+                <div>Our collection</div>
+            </div>
+        </div>
+
+    </div>
   )
 }
 
