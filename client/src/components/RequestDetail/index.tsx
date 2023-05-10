35a90159ed84232ccac99f2bbe75645e6ba83592
JeromeJULIEN <102427619+JeromeJULIEN@users.noreply.github.com>
2023-05-10 22:35:08 +0200

    page requestDetail ongoing

--- client/src/components/RequestDetail/index.tsx
+++ client/src/components/RequestDetail/index.tsx
@@ -1,9 +1,13 @@
-import React, { useMemo } from 'react'
+import React, { useMemo, useState } from 'react'
 import { Link, useNavigate, useParams } from 'react-router-dom'
 import './styles.scss'
 import BackspaceIcon from '@mui/icons-material/Backspace';
 import { useContractRead } from 'wagmi';
 import contractABI from "../../contracts/CollectorChain/CollectorChain.json"
+import ContentCopyIcon from '@mui/icons-material/ContentCopy';
+import { toast } from 'react-toastify';
+import { Modal } from '@mui/material';
+
 
 
 interface requestDetailProps {
@@ -26,14 +30,24 @@ interface NFTItem {
 
 const RequestDetail = (props: requestDetailProps) => {
   const {id} = useParams()
-
+  
+  //! :::: LOCAL STATE ::::
+  const [objPictureModal,setObjPictureModal] = useState<boolean>(false)
+  const [authPictureModal,setAuthPictureModal] = useState<boolean>(false)
+  const [storagePictureModal,setStoragePictureModal] = useState<boolean>(false)
+  
   //! :::: WAGMI ::::
   const {data : nft} : {data? : NFTItem}= useContractRead({
     address : props.contractAddress,
     abi:contractABI.abi,
     functionName : "nftList",
-    args : [id]
+    args : [id],
+    onError(error) {
+      console.log('Error', error)
+    },
   })
+  //! :::: TEST ::::
+  console.log("nft to display =>",nft, "id =>",id);
 
   //! :::: FUNCTIONS ::::
   const navigate = useNavigate()
@@ -41,21 +55,52 @@ const RequestDetail = (props: requestDetailProps) => {
     navigate(-1)
   }
 
-  const formatETHAddress = (s:any, size:number) =>{;
-    var first = s.slice(0, size + 1);
-    var last = s.slice(-size);
-    return first + "..." + last;
+  // const formatETHAddress = (s:any, size:number) =>{;
+  //   var first = s.slice(0, size + 1);
+  //   var last = s.slice(-size);
+  //   return first + "..." + last;
+  // }
+
+  // const addressToDisplay = useMemo(()=>{
+  //   return formatETHAddress(nft?.minter,3)
+  // },[nft])
+
+  const copyToClipboard = () => {
+    if (nft) {
+      navigator.clipboard.writeText(nft.minter)
+      toast.success("address copied to clipboard", {autoClose:1000})
+    } else return
   }
 
-  const addressToDisplay = useMemo(()=>{
-    return formatETHAddress(nft?.minter,3)
-  },[nft])
+  // Modal visibility management
+  const openObjectModal = () => {
+    setObjPictureModal(true)
+  }
+  const closeObjectModal = () => {
+    setObjPictureModal(false)
+  }
 
-  console.log("nft to display =>",nft);
+  const openAuthModal = () => {
+    setAuthPictureModal(true)
+  }
+  const closeAuthModal = () => {
+    setAuthPictureModal(false)
+  }
+
+  const openStorageModal = () => {
+    setStoragePictureModal(true)
+  }
+  const closeStorageModal = () => {
+    setStoragePictureModal(false)
+  }
+
+
 
   
 
   return (
+    <>
+    {nft &&
     <div className='requestDetail'>
         <h1 className='requestDetail__title'>{nft?.nftName} <button onClick={navigateToPrevious}><BackspaceIcon/></button></h1>
         <h2 className="requestDetail__status">
@@ -68,25 +113,51 @@ const RequestDetail = (props: requestDetailProps) => {
           <div className="requestDetail__item__pictures">
             <div className="requestDetail__item__pictures__picture">
               <p className="requestDetail__item__pictures__picture__text">Object picture</p>
-              <div className="requestDetail__item__pictures__picture__img"><img  src={`https://ipfs.io/ipfs/${nft?.objectImageURL}`} alt="object main" /></div>
+              <div className="requestDetail__item__pictures__picture__img"><img  src={`https://ipfs.io/ipfs/${nft?.objectImageURL}`} alt="object main" onClick={openObjectModal}/></div>
             </div>
             <div className="requestDetail__item__pictures__picture">
               <p className="requestDetail__item__pictures__picture__text">Authentification picture</p>
-              <div className="requestDetail__item__pictures__picture__img"><img  src={`https://ipfs.io/ipfs/${nft?.authImageURL}`} alt="object main" /></div>
+              <div className="requestDetail__item__pictures__picture__img"><img  src={`https://ipfs.io/ipfs/${nft?.authImageURL}`} alt="authentification" onClick={openAuthModal}/></div>
             </div>
             <div className="requestDetail__item__pictures__picture">
               <p className="requestDetail__item__pictures__picture__text">Proof of storage picture</p>
-              <div className="requestDetail__item__pictures__picture__img"><img  src={`https://ipfs.io/ipfs/${nft?.storageImageURL}`} alt="object main" /></div>
+              <div className="requestDetail__item__pictures__picture__img"><img  src={`https://ipfs.io/ipfs/${nft?.storageImageURL}`} alt="storage" onClick={openStorageModal}/></div>
             </div>
           </div>
           <div className="requestDetail__item__data">
-            <p className="requestDetail__item__data__text">Minter : {addressToDisplay}</p>
+            {/* <p className="requestDetail__item__data__text">Minter : {addressToDisplay}<ContentCopyIcon onClick={copyToClipboard}/></p> */}
             <p className="requestDetail__item__data__text">Fraction asked : {nft?.sharesQty.toString()}</p>
             {props.isAdmin && <button className="requestDetail__button requestDetail__button--big" >ACCEPT AND MINT</button>}
             {props.isAdmin && <button className="requestDetail__button requestDetail__button--big requestDetail__button--red" >REFUSE</button>}
           </div>
         </div>
     </div>
+    }
+    <Modal 
+      className="modal"
+      open={objPictureModal}
+      onClose={closeObjectModal}
+    >
+      <img src={`https://ipfs.io/ipfs/${nft?.objectImageURL}`} alt="object"  />
+      
+    </Modal>
+    <Modal 
+      className="modal"
+      open={authPictureModal}
+      onClose={closeAuthModal}
+    >
+      <img src={`https://ipfs.io/ipfs/${nft?.authImageURL}`} alt="authentification"  />
+      
+    </Modal>
+    <Modal 
+      className="modal"
+      open={storagePictureModal}
+      onClose={closeStorageModal}
+    >
+      <img src={`https://ipfs.io/ipfs/${nft?.storageImageURL}`} alt="storage"  />
+      
+    </Modal>
+    </>
     
   )
 }
